0c1182ed08a6cb00b41233d91d56794430f1959f
bekir <bekir.karahodzic@outlook.com>
2020-09-08 20:17:52 +0200

    Updates

---
+++ precache-manifest.994eeab9d26f3755f012e2a983b10e89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3714d5872ae336dbb66e4012f8f8e19",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c5b470f75e2ececd4461",
+    "url": "/my-app/static/css/main.047fc21c.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/my-app/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5b470f75e2ececd4461",
+    "url": "/my-app/static/js/main.ac4a17eb.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
